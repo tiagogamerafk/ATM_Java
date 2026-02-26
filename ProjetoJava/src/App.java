b5217fa907a8adc556f2cb4621c74b625708a26a
tiagogamerafk <tiagompereira2018@gmail.com>
2026-02-26 19:43:53 +0000

    Fixed error in withdrawl ammount, added a bit of a text design to the initial menu

--- ProjetoJava/src/App.java
+++ ProjetoJava/src/App.java
@@ -21,7 +21,8 @@ public class App {
         Scanner scanIn = new Scanner(System.in);
         int money = 0, value = 0, choice = 0;
         while (choice != -99){
-            System.out.println("[1] Check Money\n[2] Deposite Money\n[3] Withdraw Money\n[-99] Leave");
+            //System.out.println("[1] Check Money\n[2] Deposite Money\n[3] Withdraw Money\n[-99] Leave ");
+            System.out.println("--------------------\n[1] Check Money    |\n[2] Deposite Money |\n[3] Withdraw Money |\n[-99] Leave        | \n--------------------");
             choice = Integer.parseInt(scanIn.nextLine());
             if (choice == 1){
                 Check(money);
@@ -32,7 +33,12 @@ public class App {
             } else if (choice == 3){
                 System.out.println("How much do you want to withdraw?");
                 value = Integer.parseInt(scanIn.nextLine());
-                money = Withdraw(money, value);
+                if (value <= money){
+                    money = Withdraw(money, value);
+                } else {
+                    System.out.println("The value you entered is bigger than the one in your account (lowkey poor aahh nigga)");
+                }
+                
             } else if (choice == -99){
                 System.out.println("Program finished with no erros.");
                 System.exit(0);
